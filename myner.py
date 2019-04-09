bea7c53910f53f74cc92fd9008789c1edb072007
joe <cow505285@gmail.com>
2019-04-10 01:36:51 +0800

    tdm

--- myner.py
+++ myner.py
@@ -3,14 +3,15 @@ import gensim.downloader as api
 import gensim
 import spacy
 import pandas as pd
+import re
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from gensim import corpora, models, similarities
 from gensim.models import CoherenceModel
 pd.set_option("display.width",None)
 stw=stopwords.words('english')
-stw.extend(['from', 'subject', 're', 'edu', 'use','etfs','us','esg','qqq','also','pm','billion','million'])
-CLASS_NUM = 10
+stw.extend(['from', 'subject', 're', 'edu', 'use','etfs','us','esg','qqq','also','pm','billion','million','etf','market','fund','year','month','think','one'])
+CLASS_NUM = 3
 def preprocess(sentence):
 	sentence = sentence.replace(",","")
 	sentence = sentence.replace(".","")
@@ -29,6 +30,9 @@ def create_data(file_name):
 	return description_data
 def make_bigrams(texts,bigram_mod):
 	return [bigram_mod[doc] for doc in texts]
+def make_trigrams(texts,trigram_mod,bigram_mod):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+
 def lemmatization(texts,nlp ,allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
 	texts_out = []
 	for sent in texts:
@@ -60,41 +64,72 @@ def format_topics_sentences(ldamodel, corpus, texts):
 def main():
 	data_words = create_data("output.json")
 	bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)
+	trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
 	
 	bigram_mod = gensim.models.phrases.Phraser(bigram)
+	trigram_mod = gensim.models.phrases.Phraser(trigram)
 	
 	data_words_bigrams = make_bigrams(data_words,bigram_mod)
+	data_words_trigrams = make_trigrams(data_words,trigram_mod,bigram_mod)
 	
 	nlp = spacy.load('en', disable=['parser', 'ner'])
 
-	data_lemmatized = lemmatization(data_words_bigrams, nlp,allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+	data_lemmatized = lemmatization(data_words_trigrams, nlp,allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
 	
 	id2word = corpora.Dictionary(data_lemmatized)
 
-	corpus = [id2word.doc2bow(text) for text in data_words_bigrams]
+	corpus = [id2word.doc2bow(text) for text in data_words_trigrams]
 	#print([[(id2word[id], freq) for id, freq in cp] for cp in corpus[:1]])
 
 	lda_model = gensim.models.ldamodel.LdaModel(corpus = corpus,id2word = id2word,num_topics = CLASS_NUM,
-												random_state = 100,update_every = 1,chunksize = 100,passes = 10,
+												random_state = 100,update_every = 1,chunksize = 100,passes = 20,
 												alpha = 'auto',per_word_topics = True)
-	print("lda_model.topic\n",lda_model.print_topics())
-	doc_lda = lda_model[corpus]
-	print('\nPerplexity: ', lda_model.log_perplexity(corpus))
-	coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
-	coherence_lda = coherence_model_lda.get_coherence()
-	print('\nCoherence Score: ', coherence_lda)
-	df_topic_sents_keywords = format_topics_sentences(lda_model,corpus, data_lemmatized)
+	#print("lda_model.topic\n",lda_model.print_topics())
+	null_topics = lda_model.print_topics()
+	#print(type(a),a)
+	
+	classify_dict = {}
+	for row in null_topics:
+		self_type,key_word_combind = row[0],row[1]
+		key_word_list = ''.join(c for c in key_word_combind if (c.isalnum() or c.isspace()) and not c.isdigit()).split()
+		classify_dict.setdefault(self_type,key_word_list)
+	print("classify word",classify_dict)
+	with open("output.json" , 'r' ) as f:
+		output = json.load(f) 
+		tdm = []
+		for index,info in output.items():
+			temp_tdm = [0,0,0]
+			words = preprocess(info["topic"] +' ' + info['parag'])
+			words = [(w,-1) for w in words]
+			for w in words:
+				for key,val in classify_dict.items():
+					if w[0] in val:
+						temp_tdm[key] = temp_tdm[key]+1
+						w = (w[0],key)
+			tdm.append(temp_tdm)
+			info['parag'] = words
+	with open("result.json",'w') as fp:
+		json.dump(output,fp)
+	print("tdm",tdm)
+
+
+
+	#print(type(a[0][1]))
+	#doc_lda = lda_model[corpus]
+	#print('\nPerplexity: ', lda_model.log_perplexity(corpus))
+	#coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+	#coherence_lda = coherence_model_lda.get_coherence()
+	#print('\nCoherence Score: ', coherence_lda)
+	#df_topic_sents_keywords = format_topics_sentences(lda_model,corpus, data_lemmatized)
 
 # Format
-	df_dominant_topic = df_topic_sents_keywords.reset_index()
-	df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
-	print(df_dominant_topic.head(10))
+	#df_dominant_topic = df_topic_sents_keywords.reset_index()
+	#df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+	#print(df_dominant_topic.head(10))
 
-	# tfidf = models.TfidfModel(corpus)
-	# corpus_tfidf = tfidf[corpus]
-	# lda = models.LdaModel(corpus_tfidf,id2word = dic,num_topic = CLASS_NUM,alpha = 1)
-	# corpus_lda = lda[corpus_tfidf]
-	# print(lda)
+
+
+
 if __name__ == '__main__':
 	main()
 
