843334c3a0f0b7f4a5ace3449528e6c215d7ac04
lailaiforNTUIM <b04705016@ntu.edu.tw>
2019-06-16 23:29:55 +0800

    Add files via upload

--- api/score/views.py
+++ api/score/views.py
@@ -6,12 +6,14 @@ import pickle
 import os
 import numpy as np
 import pandas as pd
+import math
+import copy
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from gensim.models import word2vec
 from sklearn.svm import SVC
-from math import exp
+
 from django.conf import settings
 logging.basicConfig(level=logging.DEBUG,
 					format='%(asctime)s- %(levelname)s:%(message)s',
@@ -23,10 +25,10 @@ logging.basicConfig(level=logging.DEBUG,
 WORD_TO_VECTOR_MODEL = os.path.join(settings.DATA_DIR,"finword_to_vec.model")
 IO = os.path.join(settings.DATA_DIR,"ETF0607.xlsx")
 STOPWORD = os.path.join(settings.DATA_DIR,"stopwords.txt") 
-WORD_VECTOR_DIM = 100
+WORD_VECTOR_DIM = 20
 TITLE_LENGTH = 5
-DOC_LENGTH = 100
-
+DOC_LENGTH = 25
+THRESHOLD = 0.65
 try:
 	word_vector  = word2vec.Word2Vec.load(WORD_TO_VECTOR_MODEL).wv
 	weight_compose =  pd.read_excel(IO,2)
@@ -64,9 +66,14 @@ def model_predict(title,content,model):
 		print("shape",shape)
 		exit()
 	vote = model.predict(x_test)
-	predict = model.decision_function(x_test)
-	proba = 1/(1+exp(-1*predict))
-	return [proba,vote]
+	proba = model.predict_proba(x_test)
+	#proba = 1/(1+math.exp(-1*predict))
+	proba = max(proba[0])
+	#print(type(proba))
+	#print(proba[0][0])
+	if proba <= THRESHOLD:
+		proba = "not affected"
+	return [proba,vote[0]]
 def tfidf(title,content,idf_dict):
 	t_title = []
 	t_content = []
@@ -77,7 +84,7 @@ def tfidf(title,content,idf_dict):
 		if word not in t_title and word[0] !=" ":
 			t_title.append(word)	
 	t_title = sorted(t_title,key = lambda x:x[1],reverse = True) # sort by tfidf
-	t_title = [w[0] for w in t_title]
+	#t_title = [w[0] for w in t_title]
 	for index,word in enumerate(content):
 		w_idf = idf_dict.get(word,0)
 		tfidf = tf(word,content)*w_idf
@@ -85,18 +92,24 @@ def tfidf(title,content,idf_dict):
 		if word not in t_content and word[0] != " ":
 			t_content.append(word)
 	t_content = sorted(t_content,key = lambda x:x[1],reverse = True)
-	t_content = [w[0] for w in t_content]
+	#t_content = [w[0] for w in t_content]
 	if(len(t_title)>= TITLE_LENGTH):
+		t_title = [word_2_vec(w[0])*w[1] for w in t_title]
 		title = t_title[:TITLE_LENGTH]# each title only content title_length words
 	else:
-		dummy = (TITLE_LENGTH-len(t_title))*[" "]
-		title = t_title+dummy 
+		dummy = (TITLE_LENGTH-len(t_title))*[(" ",0)]
+		t_title = t_title+dummy
+		t_title = [word_2_vec(w[0])*w[1] for w in t_title]
+		title = np.array(t_title)
 		
 	if(len(t_content)>=DOC_LENGTH):
+		t_content = [word_2_vec(w[0])*w[1] for w in t_content]
 		content = t_content[:DOC_LENGTH]# each content only content DOC_LENGTH words
 	else:
-		dummy = (DOC_LENGTH-len(t_content))*[" "]
-		content = t_content+dummy
+		dummy = (TITLE_LENGTH-len(t_content))*[(" ",0)]
+		t_content = t_content+dummy
+		t_content = [word_2_vec(w[0])*w[1] for w in t_content]
+		content = np.array(t_content)
 	return title,content
 def tf(word,doc):
 	count = 0
@@ -105,16 +118,12 @@ def tf(word,doc):
 		if(word == w):
 			count+=1
 	return float(count)/size
-def word_2_vec(sentence,word_vector):
-	if sentence:
-		temp = []
-		for index,word in enumerate(sentence):
-			if word in word_vector.vocab and word!=" " :
-				temp.append(word_vector.wv[word])
-			else:
-				temp.append([0]*WORD_VECTOR_DIM)
-		temp = np.array(temp)			
-	return temp
+
+def word_2_vec(word):
+	if word in word_vector.vocab and word!=" " :
+		return np.array(word_vector.wv[word])
+	else:
+		return np.array([0]*WORD_VECTOR_DIM)
 class score(APIView):
 	def post(self,request):
 		
@@ -122,6 +131,7 @@ class score(APIView):
 
 		title = preprocess(request.data.get("title"))
 		content = preprocess(request.data.get("content"))
+		score_l = {}
 		ans = {}
 		if title == []or content == []:
 			return Response({"msg":"title or content can't be null"})
@@ -133,13 +143,30 @@ class score(APIView):
 			with open(COMPANY_MODEL,'rb') as f:
 				model = pickle.load(f)
 			t_title,t_content = tfidf(title.copy(),content.copy(),idf_dict)
-			t_title = word_2_vec(t_title,word_vector)
-			t_content = word_2_vec(t_content,word_vector)
-			ans[company] = model_predict(t_title,t_content,model)
-		#for i in range(weight_compose):
-
+			#t_title = word_2_vec(t_title,word_vector)
+			#t_content = word_2_vec(t_content,word_vector)
+			score_l[company] = model_predict(t_title,t_content,model)
 
 		
+
+		for i in range(len(weight_compose)):
+			temp  = copy.deepcopy(score_l)
+
+			for comp in list(temp.keys()):
+				if(temp[comp][0] == "not affected"):
+					del temp[comp]
+			for j in range(1,len(weight_compose.iloc[i])):
+				if j in temp.keys():
+					#print(temp[j])
+					if(math.isnan(weight_compose.iloc[i][j])):
+						temp[j].append(0)
+					else:
+						temp[j].append(weight_compose.iloc[i][j])
+			ans[weight_compose.iloc[i][0]] = temp
+
+			
 		
+		
+
 
 		return Response({"msg":ans})
